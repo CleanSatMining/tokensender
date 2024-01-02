aad5054d4836cd8fffb9e8c2e4da0e7c1a7a7294
Cyrille <139393003+cyrillecsm@users.noreply.github.com>
2024-01-02 10:06:24 +0100

    fix: whitelisting page

--- pages/admin/whitelisting.tsx
+++ pages/admin/whitelisting.tsx
@@ -4,6 +4,7 @@ import { Welcome } from '@/components/Welcome/Welcome';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
 import CSVUploader from '@/components/Users/Import';
 import MtPelerinSalesUploader from '@/components/Sales/MtPelerinSalesUploader';
+import DefaultLayout from '@/layouts/DefaultLayout';
 
 export default function Whitelisting() {
   const handleUpload = (data: any[]) => {
@@ -11,11 +12,11 @@ export default function Whitelisting() {
     console.log('Uploaded data:', data);
   };
   return (
-    <>
+    <DefaultLayout>
       <MtPelerinSalesUploader onUpload={handleUpload} />
       <Welcome />
       <ColorSchemeToggle />
       <CSVUploader />
-    </>
+    </DefaultLayout>
   );
 }
