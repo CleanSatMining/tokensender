ee8598ef39b71e7fc8686a79fed1b43658d5105a
Cyrille <139393003+cyrillecsm@users.noreply.github.com>
2024-01-02 11:38:23 +0100

    feat: load json

--- pages/api/getAllUserData.ts
+++ pages/api/getAllUserData.ts
@@ -2,10 +2,26 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import sqlite3 from 'sqlite3';
+import fs from 'fs/promises';
+import path from 'path';
+
+const dataFilePath = path.join(process.cwd(), 'savedData.json');
+
 /* eslint-disable */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   /* eslint-enable */
   if (req.method === 'GET') {
+    try {
+      // Charge les données depuis le fichier
+      const data = await fs.readFile(dataFilePath, 'utf-8');
+      const parsedData = JSON.parse(data);
+
+      return res.status(200).json({ success: true, data: parsedData });
+    } catch (error) {
+      console.error('Erreur lors de la lecture des données :', error);
+      return res.status(500).json({ success: false, error: 'Erreur serveur interne.' });
+    }
+
     const db = new sqlite3.Database('sales.db');
 
     // Requête pour récupérer toutes les données de la table UserData
