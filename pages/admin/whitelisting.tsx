511cab1fb36bd8ee3b7158b328042ed89c38a7d6
Cyrille <139393003+cyrillecsm@users.noreply.github.com>
2024-01-19 09:51:30 +0100

    fix

--- pages/admin/whitelisting.tsx
+++ pages/admin/whitelisting.tsx
@@ -3,17 +3,12 @@
 import { Welcome } from '@/components/Welcome/Welcome';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
 import CSVUploader from '@/components/Users/Import';
-import MtPelerinSalesUploader from '@/components/Sales/MtPelerinSalesUploader';
+
 import DefaultLayout from '@/layouts/DefaultLayout';
 
 export default function Whitelisting() {
-  const handleUpload = (data: any[]) => {
-    // Gère les données après le téléchargement si nécessaire
-    console.log('Uploaded data:', data);
-  };
   return (
     <DefaultLayout>
-      <MtPelerinSalesUploader onUpload={handleUpload} />
       <Welcome />
       <ColorSchemeToggle />
       <CSVUploader />
